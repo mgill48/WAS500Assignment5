4576495ee9d0949c9bd10b882d050e692958e8b0
mgill48 <mgill48@myseneca.ca>
2022-11-28 20:17:55 -0500

    Final Commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
     })
 );
 app.use(express.json())
-app.use(express.static('/public/images'))
+app.use(express.static(__dirname + "/public"))
 var uri = process.env.ATLAS_URI;
 //console.log(uri);
 
